3069f46e6693e8a52112bec72d8028ad98b2bf58
ryanp <ryanpanimation@gmail.com>
2020-05-25 20:25:53 -0500

    create redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, Provider } from "react-redux";
+
 import "./css/index.css";
 import App from "./App";
 
+const store = createStore();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
